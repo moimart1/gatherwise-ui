c37c32d6a5844d36da2a4bf087cedd2103120e38
moimart1 <moimart1@hotmail.fr>
2022-03-20 17:04:00 -0400

    add app frame tailwind

---
+++ src/pages/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className=''>
+      <p className='text-right'>
+        <span>Welcome, </span>
+        <span className='font-bold'>Hello you</span>
+      </p>
+      <div className='border rounded-lg my-4 p-4'>
+        <p className='py-2'>You do not belong to a company</p>
+      </div>
+    </div>
+  )
+}
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,33 @@
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './index.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        narrow: ['Oswald', 'sans-serif'], // NOTE added this custom font-narrow
+        base: ['Merriweather Sans', 'sans-serif'], // NOTE custom to cancel font-narrow
+      },
+      zIndex: {
+        100: '100',
+        110: '110',
+        120: '120',
+      },
+      colors: {
+        yellowgreen: {
+          // NOTE example of adding custom colour
+          DEFAULT: '#90ba00',
+          50: '#f9fbf4',
+          100: '#f4f8e8',
+          200: '#e8f2d2',
+          300: '#d3e3a4',
+          400: '#a7c745',
+          500: '#90ba00',
+          600: '#79aa00',
+          700: '#569400',
+          800: '#186e00',
+          900: '#002000',
+        },
+      },
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
