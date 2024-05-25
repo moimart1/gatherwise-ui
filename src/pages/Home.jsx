4ca196f7788d440b1c606f23862d5eed17d4aa41
moimart1 <moimart1@hotmail.fr>
2024-05-25 16:17:26 -0700

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,27 @@
-import React, { useContext } from 'react'
-import { LanguageContext, localize } from '../libs/localization'
+import { useNavigatePlus } from '@gatherwise/common-frontend-libs/components/LinkPlus'
+import { withAuthenticatedUserLoader } from '@gatherwise/common-frontend-libs/libs/authentication'
+import { useLoaderData } from '@gatherwise/common-frontend-libs/libs/router'
+import Loading from '@gatherwise/common-frontend-libs/pages/Loading'
+import { switchToPathFrom } from '@gatherwise/common-frontend-libs/pages/SwitchTo'
+import { getCompanyService } from '@gatherwise/common-frontend-libs/services/endpoints/companies'
+import { useEffect } from 'react'
 
-export default function Home() {
-  const lang = useContext(LanguageContext)
+export const loader = withAuthenticatedUserLoader(async ({ authenticatedUser }) => {
+  const companies = await getCompanyService().readAll(/*{ fields: ['_id'] }*/) // TODO fields when fixed in backend
+  return { authenticatedUser, companies }
+})
 
-  return <div className=''>{localize('home')(lang)}</div>
+export function Component() {
+  const { companies } = useLoaderData()
+  const navigatePlus = useNavigatePlus()
+
+  useEffect(() => {
+    navigatePlus(switchToPathFrom(companies))
+  }, [])
+
+  return (
+    <div className=''>
+      <Loading />
+    </div>
+  )
 }
--- src/pages/__tests__/Home.test.jsx
+++ src/pages/__tests__/Home.test.jsx
@@ -1,52 +1,53 @@
-import '@testing-library/jest-dom'
+import { getServiceOrigin } from '@gatherwise/common-frontend-libs/libs/urls'
+import ApplicationTestContextProvider from '@gatherwise/common-frontend-libs/test-helpers/components/ApplicationTestContextProvider'
 import { render, screen, waitFor } from '@testing-library/react'
 import axios from 'axios'
 import AxiosMockAdapter from 'axios-mock-adapter'
-import Keycloak from 'keycloak-js'
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import AuthProvider from '../../components/AuthProvider'
-import Home from '../Home'
+import { expect, test, vi } from 'vitest'
+import * as RoutedPage from '../Home'
 
 const mockRequest = new AxiosMockAdapter(axios, { delayResponse: 0 })
-jest.mock('keycloak-js')
 
-const mockContext = { useParams: {} }
-const mockedUsedNavigate = jest.fn()
-jest.mock('react-router-dom', () => ({
-  ...jest.requireActual('react-router-dom'),
-  useNavigate: () => mockedUsedNavigate,
-  useParams: () => mockContext.useParams,
-}))
-
-beforeAll(async () => {
-  //setAuthClient()
-})
+const renderPage = ({ useBackendAbility = false } = {}) => {
+  return render(
+    <ApplicationTestContextProvider
+      useBackendAbility={useBackendAbility}
+      routes={[
+        {
+          path: '/',
+          ...RoutedPage,
+        },
+        {
+          path: '/new',
+          Component: () => <div>on /new test page</div>,
+        },
+        {
+          path: '/c/id-1',
+          Component: () => <div>on /c/id-1 test page</div>,
+        },
+        {
+          path: '/switch-to',
+          Component: () => <div>on /switch-to test page</div>,
+        },
+      ]}
+      routeOptions={{
+        initialEntries: ['/'],
+      }}
+    />
+  )
+}
 
 test('Should redirect to New page', async () => {
-  Keycloak.__setAuthenticated(true)
-  mockContext.useParams = { companyId: 'id-1' }
+  vi.spyOn(window, 'location', 'get').mockReturnValue({ origin: getServiceOrigin() })
   mockRequest.onGet(new RegExp('.*/api/companies.*')).replyOnce(200, [])
 
-  render(
-    <BrowserRouter>
-      <AuthProvider clientId={'my-client-test'}>
-        <Home />
-      </AuthProvider>
-    </BrowserRouter>
-  )
-
+  renderPage()
   await waitFor(() => {
-    // Check if text present
-    expect(screen.getByText('Return to homepage')).toBeVisible()
+    expect(screen.getByText('on /new test page')).toBeInTheDocument()
   })
-
-  expect(mockedUsedNavigate).toHaveBeenCalledWith('/new')
 })
 
 test('Should redirect to company page', async () => {
-  Keycloak.__setAuthenticated(true)
-  mockContext.useParams = { companyId: 'id-1' }
   mockRequest.onGet(new RegExp('.*/api/companies.*')).replyOnce(200, [
     {
       _id: `id-1`,
@@ -55,25 +56,13 @@ test('Should redirect to company page', async () => {
     },
   ])
 
-  render(
-    <BrowserRouter>
-      <AuthProvider clientId={'my-client-test'}>
-        <Home />
-      </AuthProvider>
-    </BrowserRouter>
-  )
-
+  renderPage()
   await waitFor(() => {
-    // Check if text present
-    expect(screen.getByText('Return to homepage')).toBeVisible()
+    expect(screen.getByText('on /c/id-1 test page')).toBeInTheDocument()
   })
-
-  expect(mockedUsedNavigate).toHaveBeenCalledWith('/c/id-1')
 })
 
 test('Should redirect to Switch page', async () => {
-  Keycloak.__setAuthenticated(true)
-  mockContext.useParams = { companyId: 'id-1' }
   mockRequest.onGet(new RegExp('.*/api/companies.*')).replyOnce(200, [
     {
       _id: `id-1`,
@@ -87,18 +76,8 @@ test('Should redirect to Switch page', async () => {
     },
   ])
 
-  render(
-    <BrowserRouter>
-      <AuthProvider clientId={'my-client-test'}>
-        <Home />
-      </AuthProvider>
-    </BrowserRouter>
-  )
-
+  renderPage()
   await waitFor(() => {
-    // Check if text present
-    expect(screen.getByText('Return to homepage')).toBeVisible()
+    expect(screen.getByText('on /switch-to test page')).toBeInTheDocument()
   })
-
-  expect(mockedUsedNavigate).toHaveBeenCalledWith('/switch-to')
 })
