a71ddd98cc36cd6554bdf8d8dcd6ad0f33b5d03e
moimart1 <moimart1@hotmail.fr>
2024-05-25 16:12:54 -0700

    Update routes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,43 +1,70 @@
-import { localize } from './libs/localization'
-import Home from './pages/Home'
-import Import from './pages/Import'
-import NotFound from './pages/NotFound'
-import Settings from './pages/Settings'
-import Transactions from './pages/Transactions'
+import { withAppLayout } from '@gatherwise/common-frontend-libs/components/AppLayout'
+import { Action, Subject } from '@gatherwise/common-frontend-libs/libs/authorization'
+import { Outlet, createBrowserRouter } from '@gatherwise/common-frontend-libs/libs/router'
+import { companyPath } from '@gatherwise/common-frontend-libs/libs/urls'
+import ErrorBoundary from '@gatherwise/common-frontend-libs/pages/ErrorBoundary'
+import NotFound from '@gatherwise/common-frontend-libs/pages/NotFound'
 
-const routes = ({ lang }) => [
-  {
-    index: true,
-    path: '/',
-    title: localize('home')(lang),
-    element: <Home />,
-  },
-  {
-    path: '/settings',
-    showMenu: false,
-    title: localize('settings')(lang),
-    element: <Settings />,
-  },
-  {
-    path: '/imports',
-    menu: {
-      title: localize('Import')(lang),
-    },
-    element: <Import />,
-  },
-  {
-    path: '/transactions',
-    menu: {
-      title: localize('transactions')(lang),
-    },
-    element: <Transactions />,
-  },
-  {
-    path: '*',
-    element: <NotFound />,
-    isProtected: false,
-    wrapFrame: false,
-  },
-]
+export function getRoutes({ sitePreferences }) {
+  const withAdminAppLayout = withAppLayout({
+    sitePreferences,
+    menuMap: [
+      {
+        key: 'overview',
+        path: `${companyPath(':companyId')}`,
+        exactPath: true,
+        ability: { action: Action.Read, subject: Subject.Company },
+      },
+      {
+        key: 'users',
+        path: `${companyPath(':companyId')}/users`,
+        ability: { action: Action.ReadAllFromCompany, subject: Subject.User },
+      },
+      {
+        key: 'companies',
+        path: `${companyPath(':companyId')}/my-companies`,
+        ability: { action: Action.ReadFromParent, subject: Subject.Company },
+      },
+      {
+        key: 'settings',
+        path: `${companyPath(':companyId')}/settings`,
+      },
+    ],
+  })
+  const withAdminAppLayoutWithoutMenu = withAppLayout({ sitePreferences, menuMap: [] })
 
-export default routes
+  return [
+    {
+      id: 'root',
+      path: '/',
+      errorElement: <ErrorBoundary />,
+      Component: withAdminAppLayout(Outlet),
+      children: [
+        {
+          index: true,
+          lazy: () => import('./pages/Home'),
+        },
+        {
+          path: '/imports',
+          lazy: () => import('./pages/Import'),
+        },
+        {
+          path: '/transactions',
+          lazy: () => import('./pages/Transactions'),
+        },
+        {
+          path: '/settings',
+          lazy: async () => import('./pages/SettingsExpert'),
+        },
+      ],
+    },
+    {
+      path: '*',
+      Component: NotFound,
+    },
+  ]
+}
+
+export function getRouter({ sitePreferences }) {
+  return createBrowserRouter(getRoutes({ sitePreferences }))
+}
